8218727abbb9711893ebb8c5272935edbad77a21
rafael vincent <smurf4812@gmail.com>
2025-03-28 09:56:53 +0800

    hehe

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
-import Footer from "./components/footer";
-import Navbar from "./components/navbar";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,47 +4,40 @@ import headerLogo from "../assets/Header-logo.png";
 const Navbar = () => {
   return (
     <header className="max-w-screen-2xl mx-auto py-4">
-      <nav className="flex items-center justify-between w-full">
+      <nav className="flex items-center justify-evenly w-full">
         {/* Logo Section */}
         <div>
           <img className="w-40" src={headerLogo} alt="Company Logo" />
         </div>
 
         {/* Navigation Links */}
-        <div className="flex-grow flex justify-center">
-          <ul className="flex space-x-10 text-lg font-semibold tracking-wide uppercase">
-            <li>
-              <a href="#about" className="nav-link">
-                About
-              </a>
-            </li>
-            <li>
-              <a href="#flooring" className="nav-link">
-                Flooring
-              </a>
-            </li>
-            <li>
-              <a href="#architectural-products" className="nav-link">
-                Architectural Products
-              </a>
-            </li>
-            <li>
-              <a href="#resources" className="nav-link">
-                Resources
-              </a>
-            </li>
-            <li>
-              <a href="#blog" className="nav-link">
-                Blog
-              </a>
-            </li>
-            <li>
-              <a href="#contact" className="nav-link">
-                Contact
-              </a>
-            </li>
-          </ul>
-        </div>
+        <ul className="flex space-x-10 text-lg font-semibold tracking-wide uppercase">
+          <li>
+            <a href="#about" className="nav-link">
+              About
+            </a>
+          </li>
+          <li>
+            <a href="#flooring" className="nav-link">
+              Flooring
+            </a>
+          </li>
+          <li>
+            <a href="#architectural-products" className="nav-link">
+              Architectural Products
+            </a>
+          </li>
+          <li>
+            <a href="#resources" className="nav-link">
+              Resources
+            </a>
+          </li>
+          <li>
+            <a href="#contact" className="nav-link">
+              Contact
+            </a>
+          </li>
+        </ul>
       </nav>
     </header>
   );
